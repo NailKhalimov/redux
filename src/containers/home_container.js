bb73936bf0a38c43d98a7161a74762a51a7bfed1
Nail` Khalimov <nail980@mail.ru>
2019-01-21 14:02:05 +0300

    init

---
+++ src/containers/home_container.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import {connect} from 'react-redux';
+import { artistListAll, artistList} from '../actions';
+import {bindActionCreators} from 'redux';
+
+import Search from '../components/search';
+import ArtistList from '../components/artistlist';
+
+class HomeContainer extends Component {
+
+  componentWillMount() {
+    this.props.artistListAll();
+  }
+
+  getKeywords = (event) => {
+    let key = event.target.value;
+
+    this.props.artistList(key);
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <Search keywords={this.getKeywords} />
+        <ArtistList artists={this.props.artists.artistList}/>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    artists: state.artists
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ artistListAll, artistList }, dispatch)
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeContainer);
